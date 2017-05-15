d14cf3cd9cb26374b247e9213699f6ec2c9b71df
kimardenmiller <kimardenmiller@gmail.com>
2017-05-14 17:11:03 -0700

    Cleanup

--- Atari/dqn_atari.py
+++ Atari/dqn_atari.py
@@ -22,7 +22,7 @@ WINDOW_LENGTH = 4
 
 
 class AtariProcessor(Processor):
-    def process_observation(self, observation):
+    def process_observation(self, observation):                 # Using 'visual' image observation
         assert observation.ndim == 3  # (height, width, channel)
         img = Image.fromarray(observation)
         img = img.resize(INPUT_SHAPE).convert('L')  # resize and convert to grayscale
@@ -31,9 +31,7 @@ class AtariProcessor(Processor):
         return processed_observation.astype('uint8')  # saves storage in experience memory
 
     def process_state_batch(self, batch):
-        # We could perform this processing step in `process_observation`. In this case, however,
-        # we would need to store a `float32` array instead, which is 4x more memory intensive than
-        # an `uint8` array. This matters if we store 1M observations.
+        # Separate from `process_observation` because it needs more memory intensive `float32` array.
         processed_batch = batch.astype('float32') / 255.
         return processed_batch
 
@@ -46,13 +44,13 @@ parser.add_argument('--env-name', type=str, default='BreakoutDeterministic-v3')
 parser.add_argument('--weights', type=str, default=None)
 args = parser.parse_args()
 
-# Get the environment and extract the number of actions.
+# Get the environment and extract the number of actions. Reward equates to the on-screen score.
 env = gym.make(args.env_name)
 np.random.seed(123)
 env.seed(123)
-nb_actions = env.action_space.n
+nb_actions = env.action_space.n     # Atari Breakout has a total of 6 possible actions, 6 paddle positions
 
-# Next, we build our model. We use the same model that was described by Mnih et al. (2015).
+# Model - Mnih et al. (2015).
 input_shape = (WINDOW_LENGTH,) + INPUT_SHAPE
 model = Sequential()
 if K.image_dim_ordering() == 'tf':
@@ -76,24 +74,19 @@ model.add(Dense(nb_actions))
 model.add(Activation('linear'))
 print(model.summary())
 
-# Finally, we configure and compile our agent. You can use every built-in Keras optimizer and
-# even the metrics!
+# Configure & compile agent
 memory = SequentialMemory(limit=1000000, window_length=WINDOW_LENGTH)
 processor = AtariProcessor()
 
-# Select a policy. We use eps-greedy action selection, which means that a random action is selected
-# with probability eps. We anneal eps from 1.0 to 0.1 over the course of 1M steps. This is done so that
-# the agent initially explores the environment (high eps) and then gradually sticks to what it knows
-# (low eps). We also set a dedicated eps value that is used during testing. Note that we set it to 0.05
-# so that the agent still performs some random actions. This ensures that the agent cannot get stuck.
+# Policy. EPS-greedy for random action with probability eps.
+# Anneal eps from 1.0 to 0.1 over 1M steps, (high eps) explore to gradually sticking to what it knows (low eps).
+# Fixed 0.05 eps value during testing for some random actions to avoid agent getting stuck.
 policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1., value_min=.1, value_test=.05,
                               nb_steps=1000000)
 
-# The trade-off between exploration and exploitation is difficult and an on-going research topic.
-# If you want, you can experiment with the parameters or use a different policy. Another popular one
-# is Boltzmann-style exploration:
-# policy = BoltzmannQPolicy(tau=1.)
-# Feel free to give it a try!
+# Exploration vs. exploitation trade-off options: policy =
+#   BoltzmannQPolicy(tau=1.)
+#   LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1., value_min=.1, value_test=.05, nb_steps=1000000)
 
 dqn = DQNAgent(model=model, nb_actions=nb_actions, policy=policy, memory=memory,
                processor=processor, nb_steps_warmup=50000, gamma=.99, target_model_update=10000,
@@ -101,8 +94,7 @@ dqn = DQNAgent(model=model, nb_actions=nb_actions, policy=policy, memory=memory,
 dqn.compile(Adam(lr=.00025), metrics=['mae'])
 
 if args.mode == 'train':
-    # Okay, now it's time to learn something! We capture the interrupt exception so that training
-    # can be prematurely aborted. Notice that you can the built-in Keras callbacks!
+    # Interrupt exception capture allows premature aborted.
     weights_filename = 'dqn_{}_weights.h5f'.format(args.env_name)
     checkpoint_weights_filename = 'dqn_' + args.env_name + '_weights_{step}.h5f'
     log_filename = 'dqn_{}_log.json'.format(args.env_name)
@@ -110,10 +102,10 @@ if args.mode == 'train':
     callbacks += [FileLogger(log_filename, interval=100)]
     dqn.fit(env, callbacks=callbacks, nb_steps=1750000, log_interval=10000)
 
-    # After training is done, we save the final weights one more time.
+    # After training save in addition to check point saves.
     dqn.save_weights(weights_filename, overwrite=True)
 
-    # Finally, evaluate our algorithm for 10 episodes.
+    # Evaluate our performance for 10 episodes.
     dqn.test(env, nb_episodes=10, visualize=False)
 elif args.mode == 'test':
     weights_filename = 'dqn_{}_weights.h5f'.format(args.env_name)
--- README.md
+++ README.md
@@ -3,14 +3,9 @@
 <p align="center">
     <img src="/assets/dqn_BreakoutDeterministic-v3_170514 79_Play_wide.gif?raw=true">
 </p>
-    <table>
-      <tr>
-        <td align="center"><img src="/assets/dqn_BreakoutDeterministic-v3_170514 79_Play_wide.gif?raw=true"></td>
-      </tr>
-    </table>
 
 ## What is it?
-State-of-the art deep reinforcement learning algorithm in Tensorflow and Python for application to non-gaming environments.
+State-of-the art deep reinforcement learning algorithm in Tensorflow and Python for eventual application to non-gaming environments.
 
 ## Todos
 - Convert agent to work on MktCap / GVA application
